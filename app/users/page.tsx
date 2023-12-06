66372bcf32657c90e9b1aebe99f87cfa8cd1343b
robielcpnv <robboytm@gmail.com>
2023-12-06 11:39:52 +0100

    :zap: Fetching data from server

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,40 @@
 import React from 'react'
 
-const Userspage = () => {
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  website: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+  }
+  };
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  }
+}
+
+const Userspage = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const users: User[]  = await res.json()
   return (
-    <div>
-      UsersPage
-    </div>
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
   )
 }
 
